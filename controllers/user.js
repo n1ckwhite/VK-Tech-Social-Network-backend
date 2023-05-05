a4703be495ce539ac033271dde1678befc8560fa
Nick White <bbycinka@yandex.ru>
2023-05-05 17:30:20 +0300

    refact:(dev): рефакт login и register

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,35 @@
 const {prisma} = require('../prisma/prisma-client')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const login = (req,res) => {
+const login = async (req,res) => {
     try {
     const {email,password} = req.body
-        if(email && password) {
+        if(!email || !password) {
+            return res.status(400).json({
+                message: 'Пожалуйста, заполните обязательные поля!'
+            })
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+        const isPasswordCorrect = user && await bcrypt.compare(password,user?.password)
+        const secret = process.env.JWT_SECRET
+        if(isPasswordCorrect && user && secret) {
             res.status(200).json({
-                email,
-                password
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                token: jwt.sign({id: user.id}, secret, {expiresIn: '30d'})
             })
         } else {
             res.status(400).json({
-                message: 'Введены неправильные данные!'
+                message: 'Введен некорректный логин или пароль!'
             })
         }
-    }
-    catch {
-        res.status(500).json({
-            message: "Что-то пошло не так :("
-        })
+    } catch {
+        res.status(500).json({ message: "Что-то пошло не так" });
     }
 }
 
@@ -26,7 +37,7 @@ const register = async (req,res) => {
     try {
         const {name,email,password} = req.body
         if(!name || !email || !password) {
-            res.status(400).json({
+           return res.status(400).json({
                 message: 'Данные введены некорректно!'
             })
         }
@@ -63,12 +74,11 @@ const register = async (req,res) => {
                     token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
                 });
             } else {
-                return res.status(400).json({ message: "Не удалось создать пользователя" });
+                res.status(400).json({ message: "Не удалось создать пользователя" });
             }
         }
-    }
-    catch(error) {
-    console.log(error.message)
+    } catch {
+        res.status(500).json({ message: "Что-то пошло не так" });
     }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const {login, register} = require("../controllers/user");
 const router = express.Router()
 
 
-router.get('/login', login)
+router.post('/login', login)
 router.post('/register', register)
 
 module.exports = router
